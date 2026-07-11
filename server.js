def7e58a54adbc0aa9a5f04d47ea21c7f161d041
cheryotshamba-sudo <cheryotshamba@gmail.com>
2026-07-11 14:05:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
 const OPTIMAPAY_URL = "https://optimapaybridge.co.ke/api/v2/topup.php";
 
 
+// STK Push
 app.post("/stkpush", async (req, res) => {
 
     console.log("STK request received:", req.body);
 
-    try { 
+    try {
 
         const { phone } = req.body;
 
@@ -27,40 +29,72 @@ app.post("/stkpush", async (req, res) => {
             });
         }
 
-        const data = {
-    phone: phone,
-    amount: 100,
-    user_callback_url: "https://globallink-backend.onrender.com/callback"
-};
+
+        const response = await axios.post(
+            OPTIMAPAY_URL,
+            {
+                phone: phone,
+                amount: 100,
+                user_callback_url: "https://globallink-backend.onrender.com/callback"
+            },
             {
                 headers: {
-                    "X-API-Key": process.env.OPTIMAPAY_KEY,
-                    "X-API-Secret": process.env.OPTIMAPAY_SECRET,
+                    "X-API-KEY": process.env.OPTIMA_API_KEY,
+                    "X-API-SECRET": process.env.OPTIMA_API_SECRET,
                     "Content-Type": "application/json"
                 }
             }
         );
 
+
+        console.log("OptimaPay Response:");
+        console.log(response.data);
+
+
         res.json(response.data);
+
 
     } catch (error) {
 
-        console.log("OptimaPay Error:", error.response?.data || error.message);
+        console.log(
+            "OptimaPay Error:",
+            error.response?.data || error.message
+        );
+
 
         res.status(500).json({
-            success: false,
-            message: "Unable to send STK Push"
+            success:false,
+            message:"Unable to send STK Push"
         });
+
     }
 
 });
 
 
-app.get("/", (req, res) => {
-    res.send("GlobalLink Kenya Backend Running");
+// Callback
+app.post("/callback", (req,res)=>{
+
+    console.log("===== CALLBACK =====");
+    console.log(req.body);
+
+    res.json({
+        success:true
+    });
+
 });
 
 
-app.listen(PORT, () => {
+// Test backend
+app.get("/", (req,res)=>{
+
+    res.send("GlobalLink Kenya Backend Running");
+
+});
+
+
+app.listen(PORT, ()=>{
+
     console.log(`Server running on port ${PORT}`);
+
 });
